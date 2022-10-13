7fea70720dc5a273c57560fae91040da868c5d3c
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-10-13 12:23:59 +0100

    gas optimization constannt and immutable

--- FundMe.sol
+++ FundMe.sol
@@ -10,19 +10,19 @@ import "./PriceConverter.sol";
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50 * 1e18;
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
 
     constructor(){
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!"); // 1e18 == 1* 10 ** 18 === 1000000000000000000
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough!"); // 1e18 == 1* 10 ** 18 === 1000000000000000000
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     } 
@@ -40,7 +40,7 @@ contract FundMe {
     }
 
     modifier onlyOwner {
-        require(msg.sender == owner, "sender is not owner");
+        require(msg.sender == i_owner, "sender is not owner");
         _;
     }
 
